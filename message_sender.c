41972f4b429d32ef46977d0401900fa8ce31c6ad
rotem assa <rotemassa@mail.tau.ac.il>
2021-12-03 12:51:44 +0200

    message_sender fixed xD

--- message_sender.c
+++ message_sender.c
@@ -1,4 +1,13 @@
-//
-// Created by user on 12/2/2021.
-//
+#include <sys/fcntl.h>
+#include <sys/unistd.h>
 
+int main(int argc, char *argv[]){
+    int fd;
+    if(argc != 4){ return -1; }
+
+    fd = open(argv[1], O_RDWR);
+    //missing command: Set the channel id to the id specified on the command line.
+    write(fd, &argv[3], 128);//might need to change size of buffer 128
+    close(fd);
+    return 0;
+}
