a17bfe50d09a4557c63e0d3f5e4aff8941886853
rotem assa <rotemassa@mail.tau.ac.il>
2021-12-05 07:39:48 +0200

    added ioctl cmd to message reader and sender

--- message_reader.c
+++ message_reader.c
@@ -1,5 +1,7 @@
 #include <sys/fcntl.h>
 #include <sys/unistd.h>
+#include <sys/ioctl.h>
+
 
 int main(int argc, char *argv[]){
     int fd;
@@ -7,7 +9,7 @@ int main(int argc, char *argv[]){
     if(argc != 3){ return -1; }
 
     fd = open(argv[1], O_RDWR);
-    //missing command: Set the channel id to the id specified on the command line
+    ioctl( fd, 7, 1);
     read(fd, buf, 128);//might need to change size of buffer 128
     //Print the message to standard output (using the write() system call)
     close(fd);
