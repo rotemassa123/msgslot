cddc57f314fe24a33a90e45f16b2ba5578ce7ede
rotem assa <rotemassa@mail.tau.ac.il>
2021-12-08 13:39:07 +0200

    copied message_slot.h from recitation

--- message_slot.h
+++ message_slot.h
@@ -1,12 +1,21 @@
-//
-// Created by user on 12/2/2021.
-//
+#ifndef CHARDEV_H
+#define CHARDEV_H
 
-#include <linux/dictionary.h>
-#include <linux/kernel.h>   /* We're doing kernel work */
-#include <linux/module.h>   /* Specifically, a module */
-#include <linux/fs.h>       /* for register_chrdev */
-#include <linux/uaccess.h>  /* for get_user and put_user */
-#include <linux/string.h>   /* for memset. NOTE - not string.h!*/
+#include <linux/ioctl.h>
 
-#define DEFAULT_MAJOR 240
+// The major device number.
+// We don't rely on dynamic registration
+// any more. We want ioctls to know this
+// number at compile time.
+//#define MAJOR_NUM 244
+#define MAJOR_NUM 240
+
+// Set the message of the device driver
+#define IOCTL_SET_ENC _IOW(MAJOR_NUM, 0, unsigned long)
+
+#define DEVICE_RANGE_NAME "char_dev"
+#define BUF_LEN 80
+#define DEVICE_FILE_NAME "simple_char_dev"
+#define SUCCESS 0
+
+#endif
