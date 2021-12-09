8b3f9c5c1ac4e8c745124b3491a23236c8aab900
rotem assa <rotemassa@mail.tau.ac.il>
2021-12-09 18:48:15 +0200

    fixed message reader

--- message_sender.c
+++ message_sender.c
@@ -1,14 +1,26 @@
 #include <sys/fcntl.h>
 #include <sys/unistd.h>
 #include <sys/ioctl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define IOCTL_COMMAND_ID 8
 
 int main(int argc, char *argv[]){
-    int fd;
+    int fd, channel_number, i;
+
     if(argc != 4){ return -1; }
 
-    fd = open(argv[1], O_RDWR);
-    ioctl( fd, 7, 1);
-    write(fd, &argv[3], 128);//might need to change size of buffer 128
+    if((fd = open(argv[1], O_RDWR)) < 0) { perror("could not open file"); }
+    channel_number = atoi(argv[2]);
+
+    ioctl( fd, IOCTL_COMMAND_ID, channel_number);
+
+    for(i = 0; i < strlen(argv[3]) - 1; i++)
+        write(fd, &argv[3][i], 1);
+
     close(fd);
+
     return 0;
 }
--- message_slot.h
+++ message_slot.h
@@ -4,21 +4,15 @@
 #include <linux/ioctl.h>
 #include <linux/radix-tree.h>
 
-// The major device number.
-// We don't rely on dynamic registration
-// any more. We want ioctls to know this
-// number at compile time.
-//#define MAJOR_NUM 244
 #define MAJOR_NUM 240
 
 // Set the message of the device driver
-#define IOCTL_SET_ENC _IOW(MAJOR_NUM, 0, unsigned long)
 
 #define DEVICE_RANGE_NAME "char_dev"
 #define BUF_LEN 80
 #define DEVICE_FILE_NAME "simple_char_dev"
 #define SUCCESS 0
-#define IOCTL_SET_CHANNEL 7
+#define IOCTL_SET_CHANNEL 8
 
 typedef struct {
     char * msg;
